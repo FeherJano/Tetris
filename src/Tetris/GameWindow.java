a59adcf0f0ba1a9262c7c766699e3e80510b4c4f
FeherJano <83518660+FeherJano@users.noreply.github.com>
2023-08-17 15:21:51 +0200

    Window, Board

---
+++ src/Tetris/GameWindow.java
@@ -0,0 +1,28 @@
+package Tetris;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow {
+
+    public static final int WIDTH = 445, HEIGHT = 629;
+
+    private Board board;
+    private JFrame window;
+
+    public GameWindow() {
+        window = new JFrame("Tetris");
+        window.setSize(WIDTH, HEIGHT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+
+        board = new Board();
+        window.add(board);
+        window.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
